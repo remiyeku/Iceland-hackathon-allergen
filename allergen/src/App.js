f18f97cf76cfc2faa5153c68400f3c38e4e19b2c
Hung <hungalunga.vo@gmail.com>
2023-08-07 14:03:18 +0100

    .map now working properly

--- allergen/src/App.js
+++ allergen/src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
-import ProductList from "./Components/Product/product-list";
+import ProductList from "./components/Product/product-list";
 import React, { useState } from "react";
-import Filter from "./Components/Fillter/Filter";
+import Filter from "./components/Fillter/Filter";
+import productArray from "./db.js";
+
 
 function App() {
   const [activeAllergies, setActiveAllergies] = useState([]);
   return (
     <>
       <Filter activeAllergies={activeAllergies} />;
-      <ProductList />
+      <ProductList data={productArray} />
     </>
   );
 }
--- allergen/src/Components/Product/product-list.js
+++ allergen/src/Components/Product/product-list.js
@@ -1,37 +1,24 @@
 import React from "react";
-import "./product-list.css";
-import productArray from "../../db";
-import { useState, useEffect } from "react";
 
-//const [product, setProduct] = useState([]);
-
-console.log(typeof productArray);
-
-const ProductList = (product) => {
+const ProductList = ({ data }) => {
   return (
-    <div>
-      {product.map(function (item, i) {
-        return (
-          <div className="repeat" key={i}>
-            <div className="box">
-              <div className="product-container">
-                <div className="product-card">
-                  <img
-                    className="product-image"
-                    src={item.image}
-                    alt={item.name}
-                  />
-                  <div className="product-info">
-                    <h2 className="product-title">{item.name}</h2>
-                    <p className="product-description">{item.allergies}</p>
-                    <p>{item.price}</p>
-                  </div>
-                </div>
+    <div className="repeat">
+      {data.map((product, i) => (
+        <div key={i} className="box">
+          <div className="product-container">
+            <div className="product-card">
+              <img className="product-image" src={product.image} alt={product.name} />
+              <div className="product-info">
+                <h2 className="product-title">{product.name}</h2>
+                <p className="product-description">
+                  Allergies: {product.allergies.join(", ")}
+                </p>
+                <p className="product-price">${product.prices.toFixed(2)}</p>
               </div>
             </div>
           </div>
-        );
-      })}
+        </div>
+      ))}
     </div>
   );
 };
